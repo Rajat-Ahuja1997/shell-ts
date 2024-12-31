393a7ed4928a6b6dd154266a3dd4cdbfcfba275f
Rajat Ahuja <rahuja445@gmail.com>
2024-12-30 19:19:49 -0600

    handle echo

--- app/main.ts
+++ app/main.ts
@@ -8,13 +8,23 @@ const rl = createInterface({
 const prompt = '$ ';
 rl.setPrompt(prompt);
 
-rl.on('line', (answer) => {
-  if (answer === 'exit 0') {
-    rl.close();
-    process.exit(0);
+rl.on('line', (resp) => {
+  const [command, ...args] = resp.split(' ');
+  switch (command) {
+    case 'exit':
+      if (args.length === 1 && args[0] === '0') {
+        rl.close();
+        process.exit(0);
+      }
+      break;
+    case 'echo':
+      console.log(args.join(' '));
+      rl.prompt();
+      break;
+    default:
+      console.log(`${resp}: command not found`);
+      rl.prompt();
   }
-  console.log(`${answer}: command not found`);
-  rl.prompt();
 });
 
 rl.prompt();
