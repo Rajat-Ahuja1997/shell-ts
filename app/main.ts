682bbedba650ea01875c42eec952d485e8569520
Rajat Ahuja <rahuja445@gmail.com>
2024-12-30 19:00:53 -0600

    first

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,10 @@
-import { createInterface } from "readline";
+import { createInterface } from 'readline';
 
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-// Uncomment this block to pass the first stage
-// rl.question("$ ", (answer) => {
-//   rl.close();
-// });
+rl.question('$ ', (answer) => {
+  rl.close();
+});
